f744a59546c8c8a0b190fb04ba733f00654eb83a
Shubham Aggarwal <41572839+shubhamcompro@users.noreply.github.com>
2020-03-03 18:31:48 +0530

    init

---
+++ config/webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+        // chat: './src/chat.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new BundleAnalyzerPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'My killer app'
+        })
+    ]
+};
